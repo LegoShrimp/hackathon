67ba605cdf68baebbbd2098390de7bfe9cbf1b4c
Matthew Underwood <munde001@ucr.edu>
2013-11-24 11:44:31 -0800

    added files

--- sockets.js
+++ sockets.js
@@ -8,6 +8,14 @@ function findClientSocket ( arr, socket ) {
 	console.log('error finding socket');
 	return -1;
 }
+function findClientId ( arr, id ) {
+	for ( var i = 0; i < arr.length; ++i ){
+		if ( arr[i].id == id )
+			return i;
+	}
+	console.log('error finding socket');
+	return -1;
+}
 
 module.exports.listen = function(app){
     io = socket.listen(app);
@@ -33,7 +41,16 @@ module.exports.listen = function(app){
 		socket.on('addPoint', function(data){
 			socket.broadcast.emit('addPoint',data);
 		});
-
+		socket.on('getCanvas', function(data){
+			if (clients.length > 1){
+				clients[0].socket.emit('getCanvas',{ id:data.id});
+			}
+		});
+		socket.on('returnCanvas', function(data){
+			console.log("&&&&&&&    " + data.id );
+			var targetSocket = findClientId(clients,data.id);
+			clients[targetSocket].socket.emit('returnCanvas', { canvas_json:data.canvas_json });
+		});
 		socket.on('disconnect', function(){
 			var index = findClientSocket( clients, socket);	
 			socket.broadcast.emit('removeUser', { id:clients[index].id});
